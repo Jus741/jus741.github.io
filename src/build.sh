c94807d65eee0773d4ed061411aff373ecbd0778
Justin <88760268+Jus741@users.noreply.github.com>
2025-05-08 14:36:41 +0000

    Setup GitHub Pages

--- src/build.sh
+++ src/build.sh
@@ -13,10 +13,17 @@ if [ -f "/app/artifacts/$OUTPUT.pdf" ]; then
 	OUTPUT="resume-$(date -I)_$i"
 fi
 
+# Generate PDF
 pandoc --pdf-engine=wkhtmltopdf -s -c github-markdown-light.css -f gfm resume.md -o /app/artifacts/$OUTPUT.pdf \
     -V margin-top=10 \
     -V margin-left=10 \
     -V margin-right=10 \
     -V margin-bottom=10
 
-echo "Generated $OUTPUT.pdf"
+# Generate HTML
+pandoc -s -c github-markdown-light.css -f gfm resume.md -o /app/artifacts/index.html
+
+# Copy CSS file to artifacts
+cp github-markdown-light.css /app/artifacts/
+
+echo "Generated $OUTPUT.pdf and index.html"
